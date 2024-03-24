51b188b6aa5bb0e482fdba7118bd0ef8a9225cc2
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-03-24 23:17:56 +0530

    add font

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Nandkishor - Portfolio',
+  description: 'Nandkishor - Portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink" />
+      </head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+          {/* <Footer /> */}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header className="bg-white dark:bg-background border-b border-b-zinc-800 h-14">
+
+    </header>
+  )
+}
